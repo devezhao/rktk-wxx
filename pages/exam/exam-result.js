58e37e0c6d74276a6e39c3609ed6a8bbb26226d9
devezhao <zhaofang123@gmail.com>
2018-01-10 16:46:15 +0800

    页面优化

--- pages/exam/exam-result.js
+++ pages/exam/exam-result.js
@@ -1,11 +1,14 @@
 const app = app || getApp();
 const zutils = require('../../utils/zutils.js');
 
+import { zsharebox } from '../comps/z-sharebox.js';
 Page({
   data: {
-    hideFoobar: true
+    hideFoobar: true,
+    shareboxData: zsharebox.data
   },
   examId: null,
+  subjectId: null,
 
   onLoad: function (e) {
     this.examId = e.id;
@@ -14,32 +17,34 @@ Page({
       that.loadResult();
     });
 
-    //if (e.redirect == 1) {
-    //  this.setData({
-    //    hideFoobar: false
-    //  })
-    //}
+    // PK
+    if (e.redirect == 1) {
+      this.setData({
+        hideFoobar: false
+      })
+    }
   },
 
   loadResult: function () {
     var that = this;
     zutils.get(app, 'api/exam/result?exam=' + that.examId, function (res) {
       console.log(res);
-      var data = res.data.data;
-      var scope_percent = data.scope / data.full_scope;
+      let _data = res.data.data;
+      var scope_percent = _data.scope / _data.full_scope;
       that.setData({
-        scope: data.scope,
+        scope: _data.scope,
         scope_color: scope_percent < 0.6 ? '' : 'pass',
         scope_tips: scope_percent < 0.6 ? '本次答题未通过' : '本次答题通过',
         scope_percent: (scope_percent * 100).toFixed(1) + '%',
-        duration: data.duration,
-        full_scope: data.full_scope,
-        exam_items: data.exam_items
+        duration: _data.duration,
+        full_scope: _data.full_scope,
+        exam_items: _data.exam_items
       });
       that.__draw_circle(scope_percent);
       wx.setNavigationBarTitle({
-        title: data.subject_name
-      })
+        title: _data.subject_name
+      });
+      that.subjectId = _data.subject_id;
     });
   },
 
@@ -64,5 +69,27 @@ Page({
 
   onShareAppMessage: function () {
     return app.warpShareData();
+  },
+
+  shareboxOpen: function () {
+    zsharebox.shareboxOpen(this);
+  },
+  shareboxClose: function () {
+    zsharebox.shareboxClose(this);
+  },
+  dialogOpen: function () {
+    zsharebox.dialogOpen(this);
+  },
+  dialogClose: function () {
+    zsharebox.dialogClose(this);
+  },
+  share2Frined: function () {
+    zsharebox.share2Frined(this);
+  },
+  share2QQ: function () {
+    zsharebox.share2QQ(this);
+  },
+  share2CopyLink: function () {
+    zsharebox.share2CopyLink(this);
   }
 });
