67d6c02d7eda3de334214e916ba856ad70abac4d
devezhao <zhaofang123@gmail.com>
2018-03-30 11:50:07 +0800

    No

--- pages/index/h5.js
+++ pages/index/h5.js
@@ -12,9 +12,20 @@ Page({
 
     let url = decodeURIComponent(e.url);
     if (url.substr(0, 4) != 'http') url = zutils.baseUrl + url;
+    this.__urlRaw = url;
+
+    let that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        let ua = JSON.stringify(res);
+        url += ((url.indexOf('?') > -1 ? '&' : '?') + '_ua=' + encodeURIComponent(ua));
+        console.log('H5 加载: ' + url);
+        that.setData({ url: url });
+      },
+    })
   },
 
   onShareAppMessage: function () {
-    return app.warpShareData('/pages/index/h5?url=' + encodeURIComponent(this.data.url));
+    return app.warpShareData('/pages/index/h5?url=' + encodeURIComponent(this.__urlRaw));
   }
 })
