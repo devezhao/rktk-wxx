a2346dac0319b9373df316942aeecaad89e135ba
devezhao <zhaofang123@gmail.com>
2018-04-02 18:35:48 +0800

    H5分享参数

--- pages/index/h5.js
+++ pages/index/h5.js
@@ -12,7 +12,6 @@ Page({
 
     let url = decodeURIComponent(e.url);
     if (url.substr(0, 4) != 'http') url = zutils.baseUrl + url;
-    this.__urlRaw = url;
 
     let that = this;
     wx.getSystemInfo({
@@ -25,7 +24,23 @@ Page({
     })
   },
 
-  onShareAppMessage: function () {
-    return app.warpShareData('/pages/index/h5?url=' + encodeURIComponent(this.__urlRaw));
+  h5Message: function (e) {
+    console.log('On H5 Message - ' + JSON.stringify(e));
+  },
+
+  onShareAppMessage: function (e) {
+    let h5Url = e.webViewUrl || this.data.url;
+    let h5Url_s = h5Url.split('?');
+    let h5Url_s2 = h5Url_s[1].split('&');
+    h5Url = h5Url_s[0] + '?';
+    for (let i = 0; i < h5Url_s2.length; i++) {
+      let p = h5Url_s2[i];
+      if (p.substr(0, 2) == 'u=' || p.substr(0, 5) == 'user=' || p.substr(0, 4) == '_ua=') {
+        // 去除当前用户参数
+      } else {
+        h5Url += p + '&';
+      }
+    }
+    return app.warpShareData('/pages/index/h5?url=' + encodeURIComponent(h5Url));
   }
 })
