9d0a6aedfe37b9dde3d265df8d71b1ed250dd8ae
devezhao <zhaofang123@gmail.com>
2021-10-14 14:08:07 +0800

    1.6.1 getUserProfile

--- pages/index/auth.js
+++ pages/index/auth.js
@@ -8,38 +8,64 @@ Page({
 
   onLoad: function (e) {
     this.nexturl = decodeURIComponent(e.nexturl || '/pages/index/index');
-    wx.setNavigationBarTitle({ title: '软考大师' });
+    wx.setNavigationBarTitle({
+      title: '软考大师'
+    });
 
     let that = this;
     wx.login({
       success: function (res) {
         that.__loginCode = res.code;
-        that.setData({ authActive: true });
+        that.setData({
+          authActive: true
+        });
       }
     })
   },
 
   storeUserInfo: function (e) {
     let res = e.detail;
-    if (res.errMsg != 'getUserInfo:ok') {
-      this.setData({ authText: '重新授权' })
+    if (!(res.errMsg == 'getUserInfo:ok' || res.errMsg == 'getUserProfile:ok')) {
+      this.setData({
+        authText: '重新授权'
+      })
       return;
     }
-    this.setData({ authText: '请稍后', authActive: false })
+    this.setData({
+      authText: '请稍后',
+      authActive: false
+    })
     console.log('存储授权 - ' + JSON.stringify(res));
 
     let that = this;
-    let _data = { code: that.__loginCode, iv: res.iv, data: res.encryptedData };
+    let _data = {
+      code: that.__loginCode,
+      iv: res.iv,
+      data: res.encryptedData
+    };
     zutils.post(app, 'api/user/wxx-login?noloading', _data, function (res) {
       app.GLOBAL_DATA.USER_INFO = res.data.data;
       wx.setStorage({
-        key: 'USER_INFO', data: app.GLOBAL_DATA.USER_INFO,
+        key: 'USER_INFO',
+        data: app.GLOBAL_DATA.USER_INFO,
         success: function () {
           if (that.nexturl == 'back') wx.navigateBack()
           else app.gotoPage(that.nexturl, true)
         }
       });
     });
+  },
+
+  storeUserProfile: function (e) {
+    wx.getUserProfile({
+      lang: 'zh_CN',
+      desc: '完善用户信息',
+      success: (res) => {
+        this.storeUserInfo({
+          detail: res
+        })
+      }
+    })
   },
 
   goback() {
